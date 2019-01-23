d1c6f563a69917db4983b8da6d7aba93f38c7515
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-01-23 16:14:01 -0200

    Criar class funcionario
    Criar class funcionario.

---
+++ src/model/Funcionario.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author felipe.ferreira
+ */
+public class Funcionario {
+
+    private String nome;
+    private String cargo;
+    private Date jornadaDeTrabalho;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Date getJornadaDeTrabalho() {
+        return jornadaDeTrabalho;
+    }
+
+    public Date setJornadaDeTrabalho(Ponto ponto) throws ParseException {
+        ControlePonto controlePonto = new ControlePonto();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+        ponto.getDia();
+        String hora = "08:48";
+        Date data = (Date) format.parse(hora);
+        Time time = new Time(data.getTime());
+        data = controlePonto.formatDataHora(ponto.getDia(), time);
+
+        return data;
+    }
+}
