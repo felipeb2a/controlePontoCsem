c73c58d0af9b1d1ffafb20bf89260433bda29229
felipeb2a <felipe.m.ferreira@hotmail.com>
2019-01-23 14:19:30 -0200

    Imagens e Filepath
    Copiar icones e criar classe file path.

---
+++ src/model/FilesPath.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.io.File;
+
+/**
+ *
+ * @author felipe.ferreira
+ */
+public class FilesPath {
+    
+    //DEFINIR PASTAS PRINCIPAL
+    
+    //RC
+    private static String pastaPontoCsem = "\\\\15.0.0.3\\Team\\01. CSEM Brasil\\01. ADMINISTRATIVO-FINANCEIRO\\FINANCEIRO\\Funcionários\\Departamento Pessoal\\Folhas de Ponto\\";
+    private static String pastaPontoSunew = "\\\\15.0.0.3\\Team\\02. SUNEW\\01. ADMINISTRATIVO-FINANCEIRO\\FINANCEIRO\\Departamento Pessoal\\Folha de ponto\\";
+    private static String pastaPontoSunewGeradores = "\\\\15.0.0.3\\Team\\02. SUNEW\\01. ADMINISTRATIVO-FINANCEIRO\\FINANCEIRO\\Departamento Pessoal\\Folha de ponto\\";
+    
+     //LOG
+//    private static String pastaCsemLog = "\\\\15.0.0.3\\Team\\01. CSEM Brasil\\01. ADMINISTRATIVO-FINANCEIRO\\COMPRAS\\01. CONTROLE DE COMPRAS\\07. LOGs\\";
+//    private static String pastaSunewLog = "\\\\15.0.0.3\\Team\\02. SUNEW\\01. ADMINISTRATIVO-FINANCEIRO\\COMPRAS\\01. CONTROLE DE COMPRAS\\07. LOGs\\";
+//    private static String pastaSunewGeradoresLog = "\\\\15.0.0.3\\Team\\06. SUNEW GERADORES\\01. ADMINISTRATIVO-FINANCEIRO\\COMPRAS\\01. CONTROLE DE COMPRAS\\07. LOGs\\";
+//    
+/*###################################################################################################################################################################################*/
+    /*
+    //DEFINIR PASTAS TESTE
+    
+    /*###################################################################################################################################################################################*/
+    public static String getPastaPontoCsem() {
+        return pastaPontoCsem;
+    }
+
+    public static String getPastaPontoSunew() {
+        return pastaPontoSunew;
+    }
+
+    public static String getPastaPontoSunewGeradores() {
+        return pastaPontoSunewGeradores;
+    }
+
+//    public static String getPastaCsemLog() {
+//        return pastaCsemLog;
+//    }
+//
+//    public static String getPastaSunewLog() {
+//        return pastaSunewLog;
+//    }
+//
+//    public static String getPastaSunewGeradoresLog() {
+//        return pastaSunewGeradoresLog;
+//    }  
+}
---
+++ src/model/Icone.java
@@ -0,0 +1,33 @@
+/*@author FELIPE*/
+package model;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+public class Icone {
+    
+    private URL url;
+    
+    //ALTERAR ICONE JAVA
+    public URL getIcone(String nameDb){
+         
+       String verificaIcone [];
+       verificaIcone = nameDb.split("_");
+        if (verificaIcone[0].equals("csem")) {
+            url = this.getClass().getResource("/images/icon_system_csem.png");
+        }
+        if (verificaIcone[0].equals("sunew")) {
+            url = this.getClass().getResource("/images/icon_system_sunew.png");
+        }
+        if (verificaIcone[0].equals("sunewgeradores")) {
+            url = this.getClass().getResource("/images/icon_system_sunew_geradores.png");
+        }        
+        return url;
+    }
+    
+    public void setIcone(URL url){
+        this.url = url;
+    }
+    
+}
